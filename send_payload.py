b3e23c3b5ad3d14a614d27c4fd64be629b9fde74
FXmario <fransiskus_mario87@outlook.com>
2024-08-15 13:37:11 +0700

    Update send payload

--- send_payload.py
+++ send_payload.py
@@ -1,19 +1,28 @@
 import requests
 import sys
 import numpy as np
+import re
 
 
-def generate_payload(url, file):
-    open_payloads = open(file, 'r')
+def generate_payload(url_file, payload_file):
+    open_urls = open(url_file, 'r')
+    urls = np.array(open_urls.readlines())
+
+    open_payloads = open(payload_file, 'r')
     payloads = np.array(open_payloads.readlines())
 
-    for payload in payloads:
-        url_payload = f"{url}{payload}"
-        print(f"URL: {url_payload}")
-        response = requests.get(
-            url=url_payload
-        )
-        print(response.text)
+    for url in urls:
+        print(url)
+        for payload in payloads:
+            url_payload = re.sub(r'=(.*?)(?=&|$)', f'={payload}', url)
+            print(f"URL: {url_payload}")
+            response = requests.get(
+                url=url_payload
+            )
+            result = response.text
+            print(result)
+            with open("result_payload.txt", "a") as output:
+                output.write(f"URL: {url_payload}\n{result}\n\n")
 
 
 generate_payload(sys.argv[1], sys.argv[2])
